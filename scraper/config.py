d49e8dba40d782d2e209da51338a1f7fc16ff221
Samuel Muniz <samvmvniz@gmail.com>
2026-04-25 14:06:31 -0300

    fix: keywords_default genericas para todas as areas (nao so educacao)

--- scraper/config.py
+++ scraper/config.py
@@ -7,9 +7,26 @@ SUPABASE_URL = os.getenv("SUPABASE_URL", "")
 SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY", "")
 
 KEYWORDS_DEFAULT = [
-    "professor", "docente", "licenciatura", "ciências sociais", "história",
-    "matemática", "tecnologia educacional", "proatec", "edtech",
-    "educação", "ensino", "pedagogia",
+    # Tecnologia
+    "desenvolvedor", "programador", "analista de sistemas", "data science",
+    "suporte técnico", "infraestrutura", "segurança da informação",
+    # Administração e negócios
+    "administrativo", "assistente administrativo", "recursos humanos",
+    "financeiro", "contabilidade", "controladoria", "analista financeiro",
+    # Direito
+    "advogado", "assistente jurídico", "analista jurídico", "paralegal",
+    # Saúde
+    "enfermeiro", "técnico de enfermagem", "farmacêutico", "fisioterapeuta",
+    # Educação
+    "professor", "pedagogo", "coordenador pedagógico", "educação",
+    # Engenharia
+    "engenheiro civil", "engenheiro elétrico", "técnico em edificações",
+    # Comunicação e marketing
+    "marketing digital", "social media", "redator", "jornalista",
+    # Vendas e atendimento
+    "vendedor", "representante comercial", "atendimento ao cliente",
+    # Geral
+    "estágio", "jovem aprendiz", "trainee", "analista",
 ]
 
 REQUEST_DELAY_SECONDS = 1
